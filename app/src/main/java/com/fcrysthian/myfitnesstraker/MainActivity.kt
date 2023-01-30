ffa0867a3aef421fd70d2c766a5a471d69204abd
Felýpë Costa <rebelo.filipe020@gmail.com>
2023-01-30 17:05:45 -0400

    implementacao da acao de click para os botoes

--- app/src/main/java/com/fcrysthian/myfitnesstraker/MainActivity.kt
+++ app/src/main/java/com/fcrysthian/myfitnesstraker/MainActivity.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
@@ -42,58 +44,69 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        mainItems.add(
-            MainItem(
-                id = 3,
-                drawableId = R.drawable.ic_baseline_water_drop_24,
-                textStringId = R.string.tmb,
-                color = Color.DKGRAY
-            )
-        )
+        val adapter = MainAdapter(mainItems){id ->
+            when (id) {
+                1 -> {
+                    val intent = Intent(this@MainActivity, ImcActivity::class.java)
+                    startActivity(intent)
+                }
+                2 -> {
+                    // abrir uma outra activity
+                }
+            }
+            Log.i("Teste", "clicou $id!!")
+        }
 
-        mainItems.add(
-            MainItem(
-                id = 4,
-                drawableId = R.drawable.ic_baseline_water_drop_24,
-                textStringId = R.string.tmb,
-                color = Color.MAGENTA
-            )
-        )
-
-        val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
         rvMain.layoutManager = GridLayoutManager(this,2 )
 
     }
 
-    private inner class MainAdapter(private val mainItem: List<MainItem>) : RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(
+        private val mainItems: List<MainItem>,
+//        private val onItemClickListener: OnItemClickListener
+        private val onItemClickListener: (Int) -> Unit,
+    ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+        // 1 - Qual é o layout XML da celula especifica (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.recycler_main, parent, false)
             return MainViewHolder(view)
         }
 
+        // 2 - disparado toda vez houver uma rolagem na tela e for necessario trocar o conteudo
+        // da celula
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-            val itemCurrent = mainItem[position]
+            val itemCurrent = mainItems[position]
             holder.bind(itemCurrent)
         }
 
+        // 3 - informar quantas celulas essa listagem terá
         override fun getItemCount(): Int {
-            return mainItem.size
+            return mainItems.size
         }
 
-    }
+        // é a classe da celula em si!!!
+        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: MainItem) {
+                val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+                val name: TextView = itemView.findViewById(R.id.item_txt_name)
+                val container: LinearLayout = itemView.findViewById(R.id.item_container_imc)
 
-    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: MainItem){
-            val img: ImageView = itemView.findViewById(R.id.item_img_icon)
-            val name: TextView = itemView.findViewById(R.id.item_txt_name)
-            val container: LinearLayout = itemView.findViewById(R.id.item_container_imc)
+                img.setImageResource(item.drawableId)
+                name.setText(item.textStringId)
+                container.setBackgroundColor(item.color)
 
-            img.setImageResource(item.drawableId)
-            name.setText(item.textStringId)
-            container.setBackgroundColor(item.color)
+                container.setOnClickListener {
+                    // aqui ele é uma ref. function
+                    onItemClickListener.invoke(item.id)
 
+                    // aqui ele é uma ref. interface
+                    // onItemClickListener.onClick(item.id)
+                }
+            }
         }
+
     }
 }
